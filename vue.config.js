00f172f98417cd67b13b0f4dc16db955f6aa3434
Andrey Sokolov <andrey.sokolov@jpower8.cz>
2022-05-01 13:48:03 +0200

    Add svg optimization

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+
+    // add replacement loader(s)
+    svgRule
+        .use('svgo-loader')
+        .loader('svgo-loader')
+        .tap(() => {
+          return {
+            removeStyleElement: true,
+          }
+        })
+  }
+}
